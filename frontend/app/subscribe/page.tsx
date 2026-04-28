6142f15a9b5f09246b30025ac6bf7ef50cd074f2
toonyvu <tony422005@gmail.com>
2026-04-28 15:07:10 +0700

    Feature: Completed Basic Stripe Integration

---
+++ frontend/app/subscribe/page.tsx
@@ -0,0 +1,11 @@
+import SubscribeLeft from "@/components/SubscribeLeft";
+
+export default async function SubscribePage() {
+  return (
+    <div className="flex-1 flex">
+      <SubscribeLeft />
+
+      <div className="flex-1">Hi</div>
+    </div>
+  );
+}
